a90a88b9f924a602007058ba3b732bca9d0e2b88
pravinsamr1 <pravinsamr@gmail.com>
2026-02-12 11:37:56 +0530

    changeddd

--- assets/js/custom-select.js
+++ assets/js/custom-select.js
@@ -2,20 +2,20 @@ $(document).ready(function () {
     $('#yearSelect').select2({
         placeholder: '--Select Year--',
         minimumResultsForSearch: 0,
-        width: '91%'
+        width: '100%'
     });
 });
 $(document).ready(function () {
     $('#themeSelect').select2({
         placeholder: '--Select Theme--',
         minimumResultsForSearch: 0,
-        width: '88%'
+        width: '100%'
     });
 });
 $(document).ready(function () {
     $('#countrySelect').select2({
         placeholder: '--Select Country--',
         minimumResultsForSearch: 0,
-        width: '88%'
+        width: '100%'
     });
 });
